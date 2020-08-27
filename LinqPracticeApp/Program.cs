6ff7254505608d71826fe495b5f058913794b127
MandasK <king.amanda.r@gmail.com>
2020-08-27 14:18:31 -0500

    L word exercise

--- LinqPracticeApp/Program.cs
+++ LinqPracticeApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqPracticeApp
 {
@@ -6,7 +8,21 @@ namespace LinqPracticeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Find the words in the collection that start with the letter ' L
+
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+            where fruit[0] == 'L'
+            select fruit;
+
+            Console.WriteLine("These Guys are L'Licious.");
+            Console.WriteLine();
+            foreach (string L in LFruits)
+            {
+                Console.WriteLine($"{L}");
+            }
+
         }
     }
 }
