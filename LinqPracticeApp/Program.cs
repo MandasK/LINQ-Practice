3b7edd27a8b1128d949e53348c05f0b1b0ff08a5
MandasK <king.amanda.r@gmail.com>
2020-08-27 14:33:49 -0500

    multipliers and descending complete

--- LinqPracticeApp/Program.cs
+++ LinqPracticeApp/Program.cs
@@ -16,13 +16,76 @@ namespace LinqPracticeApp
             where fruit[0] == 'L'
             select fruit;
 
-            Console.WriteLine("These Guys are L'Licious.");
+            Console.WriteLine("These Guys are L'Licious:");
             Console.WriteLine();
             foreach (string L in LFruits)
             {
                 Console.WriteLine($"{L}");
             }
 
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("These guys are MULTIPLYING! It's Electrifying! (but only sixeys and foursies)");
+            foreach (int num in fourSixMultiples)
+            {
+                Console.WriteLine(num);
+            }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather",
+                "James",
+                "Xavier",
+                "Michelle",
+                "Brian",
+                "Nina",
+                "Kathleen",
+                "Sophia",
+                "Amir",
+                "Douglas",
+                "Zarley",
+                "Beatrice",
+                "Theodora",
+                "William",
+                "Svetlana",
+                "Charisse",
+                "Yolanda",
+                "Gregorio",
+                "Jean-Paul",
+                "Evangelina",
+                "Viktor",
+                "Jacqueline",
+                "Francisco",
+                "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending(name => name).ToList();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Show me some names in Reverse Alphabet!");
+            descend.ForEach(name => Console.WriteLine($"{name}"));
         }
     }
 }
